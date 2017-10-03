4f632f52526e9d50eceec5fcadf1d63c03b3a280
UMB200 <tomodachi200@gmail.com>
2017-10-03 14:22:13 -0400

    upload solution and html file with link to js file

--- noRepeat.js
+++ noRepeat.js
@@ -1,6 +1,33 @@
 
 function permAlone(str) {
-  return str;
+	//split all parameters and put them into array placeholder
+	var placeHolder = str.split('');
+	//count every time appropriate combination is available
+	var count = 0;
+
+	//swap items in array to go through all combinations
+	function swap(a,b){
+		var switcher = placeHolder[a];
+		placeHolder[a] = placeHolder[b];
+		placeHolder[b] = switcher;
+	}
+	//idea is based on Heap's algorithm to go trhough all possible combinations 
+	function permute(z){
+		var target = /([a-z])\1+/;
+
+		if(z === 1 && !target.test(placeHolder.join(''))){
+			count++;
+		}
+		else {
+			for (var i = 0; i != z; i++) {
+				permute(z-1);
+				swap(z % 2 ? 0: i, z-1);
+			}
+		}
+	}
+	//recursive function
+	permute(placeHolder.length);
+  return count;
 }
 
 permAlone('aab');
